9ec21a307e56518fb3f6459085f62f5cd12cd780
chris <1040701709@qq.com>
2019-11-12 17:59:33 +0800

    rank data

--- src/main/resources/static/js/ranks.js
+++ src/main/resources/static/js/ranks.js
@@ -45,7 +45,81 @@ function init() {
                         type: 'bar'
                     }]
                 };
-                this.songGraph.setOption(songOption);
+
+                var myColor = ['#eb2100', '#eb3600', '#d0570e', '#d0a00e', '#34da62', '#00e9db', '#00c0e9', '#0096f3', '#33CCFF', '#33FFCC'];
+                var name1=[];
+                var rank1=[];
+                for(var i=0;i<this.songGraphData.length;i++){
+                    name1[i]=this.songGraphData[i].name;
+                    rank1[i]=this.songGraphData[i].rank;
+                }
+
+                let option = {
+                    backgroundColor: '#0e2147',
+                    grid: {
+                        left: '25%',
+                        top: '12%',
+                        right: '25%',
+                        bottom: '8%',
+                        containLabel: true
+                    },
+                    xAxis: [{
+                        show: false,
+                    }],
+                    yAxis: [{
+                        axisTick: 'none',
+                        axisLine: 'none',
+                        offset: '27',
+                        axisLabel: {
+                            textStyle: {
+                                color: '#ffffff',
+                                fontSize: '16',
+                            }
+                        },
+                        data: name1
+                    }, {
+                        name: '歌曲排行',
+                        nameGap: '30',
+                        nameTextStyle: {
+                            color: '#ffffff',
+                            fontSize: '30',
+                        },
+                        axisLine: {
+                            lineStyle: {
+                                color: 'rgba(0,0,0,0)'
+                            }
+                        },
+                        data: [],
+                    }],
+                    series: [{
+                        name: '条',
+                        type: 'bar',
+                        yAxisIndex: 0,
+                        data: rank1,
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'right',
+                                textStyle: {
+                                    color: '#ffffff',
+                                    fontSize: '16',
+                                }
+                            }
+                        },
+                        barWidth: 12,
+                        itemStyle: {
+                            normal: {
+                                color: function(params) {
+                                    var num = myColor.length;
+                                    return myColor[params.dataIndex % num]
+                                },
+                            }
+                        },
+                        z: 2
+                    }]
+                };
+
+                this.songGraph.setOption(option);
 
                 //playlist render
                 this.playlistGraph=echarts.init(document.getElementById("playlist"));
@@ -73,11 +147,81 @@ function init() {
                     }]
 
                 };
-                this.playlistGraph.setOption(playlistOption);
+                var name2=[];
+                var rank2=[];
+                for(var i=0;i<this.playlistGraphData.length;i++){
+                    name2[i]=this.playlistGraphData[i].name;
+                    rank2[i]=this.playlistGraphData[i].rank;
+                }
+
+                let option2 = {
+                    backgroundColor: '#0e2147',
+                    grid: {
+                        left: '25%',
+                        top: '12%',
+                        right: '25%',
+                        bottom: '8%',
+                        containLabel: true
+                    },
+                    xAxis: [{
+                        show: false,
+                    }],
+                    yAxis: [{
+                        axisTick: 'none',
+                        axisLine: 'none',
+                        offset: '27',
+                        axisLabel: {
+                            textStyle: {
+                                color: '#ffffff',
+                                fontSize: '16',
+                            }
+                        },
+                        data: name2
+                    }, {
+                        name: '歌单排行',
+                        nameGap: '30',
+                        nameTextStyle: {
+                            color: '#ffffff',
+                            fontSize: '30',
+                        },
+                        axisLine: {
+                            lineStyle: {
+                                color: 'rgba(0,0,0,0)'
+                            }
+                        },
+                        data: [],
+                    }],
+                    series: [{
+                        name: '条',
+                        type: 'bar',
+                        yAxisIndex: 0,
+                        data: rank2,
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'right',
+                                textStyle: {
+                                    color: '#ffffff',
+                                    fontSize: '16',
+                                }
+                            }
+                        },
+                        barWidth: 12,
+                        itemStyle: {
+                            normal: {
+                                color: function(params) {
+                                    var num = myColor.length;
+                                    return myColor[params.dataIndex % num]
+                                },
+                            }
+                        },
+                        z: 2
+                    }]
+                };
+                this.playlistGraph.setOption(option2);
 
                 //level render
                 this.levelGraph=echarts.init(document.getElementById("level"));
-                var levelData=[];
 
                 let levelOption = {
                     tooltip: {
@@ -103,7 +247,79 @@ function init() {
                     }]
 
                 };
-                this.levelGraph.setOption(levelOption);
+
+                var name3=[];
+                var rank3=[];
+                for(var i=0;i<this.levelGraphData.length;i++){
+                    name3[i]=this.levelGraphData[i].name;
+                    rank3[i]=this.levelGraphData[i].level;
+                }
+
+                let option3 = {
+                    backgroundColor: '#0e2147',
+                    grid: {
+                        left: '25%',
+                        top: '12%',
+                        right: '25%',
+                        bottom: '8%',
+                        containLabel: true
+                    },
+                    xAxis: [{
+                        show: false,
+                    }],
+                    yAxis: [{
+                        axisTick: 'none',
+                        axisLine: 'none',
+                        offset: '27',
+                        axisLabel: {
+                            textStyle: {
+                                color: '#ffffff',
+                                fontSize: '16',
+                            }
+                        },
+                        data: name3
+                    }, {
+                        name: '等级排行',
+                        nameGap: '30',
+                        nameTextStyle: {
+                            color: '#ffffff',
+                            fontSize: '30',
+                        },
+                        axisLine: {
+                            lineStyle: {
+                                color: 'rgba(0,0,0,0)'
+                            }
+                        },
+                        data: [],
+                    }],
+                    series: [{
+                        name: '条',
+                        type: 'bar',
+                        yAxisIndex: 0,
+                        data: rank3,
+                        label: {
+                            normal: {
+                                show: true,
+                                position: 'right',
+                                textStyle: {
+                                    color: '#ffffff',
+                                    fontSize: '16',
+                                }
+                            }
+                        },
+                        barWidth: 12,
+                        itemStyle: {
+                            normal: {
+                                color: function(params) {
+                                    var num = myColor.length;
+                                    return myColor[params.dataIndex % num]
+                                },
+                            }
+                        },
+                        z: 2
+                    }]
+                };
+                this.levelGraph.setOption(option3);
 
                 console.log("end rendering")
 
