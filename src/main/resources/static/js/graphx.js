d4bc9a177f4786c9daa11e0bc90cf3d2c8532cf5
chris <1040701709@qq.com>
2019-11-12 15:52:55 +0800

    graphx

--- src/main/resources/static/js/graphx.js
+++ src/main/resources/static/js/graphx.js
@@ -9,64 +9,91 @@ function init() {
             dataTimer:"",
             renderTimer:"",
 
+            categories:[
+                {
+                    name:"playlist"
+                },
+                {
+                    name:"user"
+                },
+                {
+                    name:"song"
+                }],
             graph1Data:[],
             graph1Edge:[],
             graph2Data:[],
             graph2Edge:[],
-            graph3Data:[],
-            graph3Edge:[],
-            graph4Data:[],
-            graph4Edge:[],
+
 
             graph1:"",
             graph2:"",
-            graph3:"",
-            graph4:"",
+
         },
         methods:{
             renderGraph: function () {
                 console.log("start rendering!");
 
                 //graph1 render
-                var graph1DataSet=["user","song","playlist"];
                 this.graph1=echarts.init(document.getElementById("graph1"));
                 this.graph1Data.forEach(function (node) {
+                    node.id=JSON.stringify(node.nodeId);
                     node.itemStyle = null;
-                    node.symbolSize = 10;
                     node.value = node.symbolSize;
-                    node.category = 0;
+                    switch (node.prop) {
+                        case 'playlist':
+                            // code
+                            node.category=0;
+                            node.symbolSize=10;
+                            break;
+                        case 'user':
+                            // code
+                            node.category=1;
+                            node.symbolSize=20;
+                            break;
+                        case 'song':
+                            // code
+                            node.category=2;
+                            node.symbolSize=30;
+                            break;
+                    }
                     // Use random x, y
                     node.x = node.y = null;
-                    node.draggable = true;
+                    node.draggable = false;
                 });
+                var id=0;
                 this.graph1Edge.forEach(function (edge) {
-                   edge.source=edge.srcId;
-                   edge.target=edge.dstId;
+                    edge.id=JSON.stringify(id);
+                    edge.source=JSON.stringify(edge.srcId);
+                    edge.target=JSON.stringify(edge.dstId);
+                    id+=1;
                 });
-                console.log()
+                console.log(this.graph1Data)
+                console.log(this.graph1Edge)
                 let graph1Option = {
                     title: {
-                        text: '评论',
-                        subtext: '网易云',
+                        text: 'top歌曲-歌单',
+                        subtext: '有向关系图',
                         top: 'top',
                         left: 'center'
                     },
                     tooltip: {},
                     legend: [{
                         // selectedMode: 'single',
-                        data: graph1DataSet.map(function (a) {
-                            return a;
-                        })
+                        data: this.categories.map(function (a) {
+                            return a.name;
+                        }),
+                        top: 'bottom',
+                        left: 'right'
                     }],
                     animation: false,
                     series : [
                         {
-                            name: '评论',
+                            name: '',
                             type: 'graph',
                             layout: 'force',
                             data: this.graph1Data,
                             links: this.graph1Edge,
-                            categories: graph1DataSet,
+                            categories: this.categories,
                             roam: true,
                             label: {
                                 normal: {
@@ -81,31 +108,66 @@ function init() {
                 };
                 this.graph1.setOption(graph1Option);
                 //graph2 render
-                var graph2DataSet=[];
                 this.graph2=echarts.init(document.getElementById("graph2"));
+                this.graph2Data.forEach(function (node) {
+                    node.id=JSON.stringify(node.nodeId);
+                    node.itemStyle = null;
+                    node.symbolSize = 10;
+                    node.value = node.symbolSize;
+                    switch (node.prop) {
+                        case 'playlist':
+                            // code
+                            node.category=0;
+                            node.symbolSize=30;
+                            break;
+                        case 'user':
+                            // code
+                            node.category=1;
+                            node.symbolSize=20;
+                            break;
+                        case 'song':
+                            // code
+                            node.category=2;
+                            node.symbolSize=10;
+                            break;
+                    }
+                    // Use random x, y
+                    node.x = node.y = null;
+                    node.draggable = false;
+                });
+                var id=0;
+                this.graph2Edge.forEach(function (edge) {
+                    edge.id=JSON.stringify(id);
+                    edge.source=JSON.stringify(edge.srcId);
+                    edge.target=JSON.stringify(edge.dstId);
+                    id+=1;
+                });
+
                 let graph2Option = {
                     title: {
-                        text: '评论',
-                        subtext: '网易云',
+                        text: 'top歌单-歌曲',
+                        subtext: '有向关系图',
                         top: 'top',
                         left: 'center'
                     },
                     tooltip: {},
                     legend: [{
                         // selectedMode: 'single',
-                        data: graph1DataSet.map(function (a) {
-                            return a;
-                        })
+                        data: this.categories.map(function (a) {
+                            return a.name;
+                        }),
+                        top: 'bottom',
+                        left: 'right'
                     }],
                     animation: false,
                     series : [
                         {
-                            name: '评论',
+                            name: '',
                             type: 'graph',
                             layout: 'force',
-                            data: this.graph1Data,
-                            links: this.graph1Edge,
-                            categories: graph1DataSet,
+                            data: this.graph2Data,
+                            links: this.graph2Edge,
+                            categories: this.categories,
                             roam: true,
                             label: {
                                 normal: {
@@ -118,86 +180,8 @@ function init() {
                         }
                     ]
                 };
-                this.graph2.setOption(graph2Option);
-                //graph3 render
-                var graph3DataSet=[];
-                this.graph3=echarts.init(document.getElementById("graph3"));
-                let graph3Option = {
-                    title: {
-                        text: '评论',
-                        subtext: '网易云',
-                        top: 'top',
-                        left: 'center'
-                    },
-                    tooltip: {},
-                    legend: [{
-                        // selectedMode: 'single',
-                        data: graph1DataSet.map(function (a) {
-                            return a;
-                        })
-                    }],
-                    animation: false,
-                    series : [
-                        {
-                            name: '评论',
-                            type: 'graph',
-                            layout: 'force',
-                            data: this.graph1Data,
-                            links: this.graph1Edge,
-                            categories: graph1DataSet,
-                            roam: true,
-                            label: {
-                                normal: {
-                                    position: 'right'
-                                }
-                            },
-                            force: {
-                                repulsion: 100
-                            }
-                        }
-                    ]
-                };
-                this.graph3.setOption(graph3Option);
-                //graph4 render
-                var graph4DataSet=[];
-                this.graph4=echarts.init(document.getElementById("graph4"));
-                let graph4Option = {
-                    title: {
-                        text: '评论',
-                        subtext: '网易云',
-                        top: 'top',
-                        left: 'center'
-                    },
-                    tooltip: {},
-                    legend: [{
-                        // selectedMode: 'single',
-                        data: graph1DataSet.map(function (a) {
-                            return a;
-                        })
-                    }],
-                    animation: false,
-                    series : [
-                        {
-                            name: '评论',
-                            type: 'graph',
-                            layout: 'force',
-                            data: this.graph1Data,
-                            links: this.graph1Edge,
-                            categories: graph1DataSet,
-                            roam: true,
-                            label: {
-                                normal: {
-                                    position: 'right'
-                                }
-                            },
-                            force: {
-                                repulsion: 100
-                            }
-                        }
-                    ]
-                };
-                this.graph4.setOption(graph4Option);
 
+                this.graph2.setOption(graph2Option);
 
                 console.log("end rendering")
 
@@ -206,25 +190,25 @@ function init() {
                 console.log("start init");
                 this.getGraph1Data();
                 this.getGraph2Data();
-                this.getGraph3Data();
-                this.getGraph4Data();
                 console.log("Data init complete!");
 
             },
             getGraph1Data:function () {
-                this.$http.get("/graph/getAddNodes").then(function (response) {
+                this.$http.get("/graph/getTopSongNodes").then(function (response) {
                     this.graph1Data=response.data;
                 });
-                this.$http.get("/graph/getAddEdges").then(function (response) {
+                this.$http.get("/graph/getTopSongEdges").then(function (response) {
                     this.graph1Edge=response.data;
-                })
+                });
+
+
             },
             getGraph2Data:function () {
-                this.$http.get("/graph/getTopSongNodes").then(function (response) {
+                this.$http.get("/graph/getTopListNodes").then(function (response) {
                     this.graph2Data=response.data;
 
                 });
-                this.$http.get("/graph/getTopSongEdges").then(function (response) {
+                this.$http.get("/graph/getTopListEdges").then(function (response) {
                     this.graph2Edge=response.data;
                 });
             },
@@ -236,18 +220,16 @@ function init() {
                     this.graph3Edge=response.data
                 });
             },
-            getGraph4Data:function () {
-
-            },
 
         },
         mounted:function () {
 
-            this.dataTimer=setInterval(this.initial,5000);
-            this.renderTimer=setInterval(this.renderGraph,5000);
+            // this.dataTimer=setInterval(this.initial,5000);
+            // this.renderTimer=setInterval(this.renderGraph,5000);
 
-            // this.initial();
-            // this.renderGraph()
+            this.initial();
+            // this.renderGraph();
+            this.renderTimer=setTimeout(this.renderGraph,10000);
         }
     });
 
